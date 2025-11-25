761e7b5d65dccfb23d0ee946eca830071373afd1
parsa.ghafari <pghm1384@gmail.com>
2025-11-25 13:40:50 +0330

    get all data from program.dart static variable instead of sharedPreferences

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hello_trip/components/program.dart';
 import 'package:hello_trip/pages/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -9,19 +14,60 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-// flutter_tts
-// speech_to_text
-// google_cloude_translation
-
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Future.delayed(const Duration(milliseconds: 3000), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => HomePage(selectedIndex: 2)),
-      );
-    });
     super.initState();
+    initApp();
+  }
+
+  Future<void> initApp() async {
+    await loadSharedPhrefences();
+    await loadJsonDataEssential();
+    await loadJsonDataTerms();
+
+    print("=== All Loaded === ");
+
+    // Future.delayed(const Duration(milliseconds: 3000), () {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => HomePage(selectedIndex: 2)),
+    );
+    // });
+  }
+
+  Future<void> loadSharedPhrefences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final orginLanguage = prefs.getString("orgin_lang") ?? "fa";
+    final accountName = prefs.getString("account_name") ?? "Unknown";
+    final targetLanguage = prefs.getString("target_lang") ?? "en";
+    setState(() {
+      // ignore: unused_local_variable
+      program.OrginLang = orginLanguage;
+      program.name = accountName;
+      program.TargetLang = targetLanguage;
+    });
+  }
+
+  Future<void> loadJsonDataEssential() async {
+    final String jsonString = await rootBundle.loadString(
+      'assets/json/essential.json',
+    );
+    var data = jsonDecode(jsonString);
+
+    setState(() {
+      program.DataEssential = data;
+    });
+  }
+
+  Future<void> loadJsonDataTerms() async {
+    final String jsonString = await rootBundle.loadString(
+      'assets/json/terms.json',
+    );
+    var data = jsonDecode(jsonString);
+
+    setState(() {
+      program.DataTerms = data;
+    });
   }
 
   @override
